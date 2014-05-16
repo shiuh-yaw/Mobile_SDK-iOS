40434a2f66e0356e79fdaec2d7475d5f1b8f1646
ZooLisOne <xul.xi.xuan@gmail.com>
2014-05-16 21:14:14 +0800

    Mobile_SDK_4.0-iOS

---
+++ MOLPaySDK Example/MOLPaySDK Example/MOLPayExampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOLPayExampleAppDelegate.h
+//  MOLPaySDK Example
+//
+//  Created by MOLPay on 5/16/14.
+//  Copyright (c) 2014 MOLPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOLPayExampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MOLPaySDK Example/MOLPaySDK Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MOLPaySDK Example
+//
+//  Created by MOLPay on 5/16/14.
+//  Copyright (c) 2014 MOLPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOLPayExampleAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOLPayExampleAppDelegate class]));
+    }
+}
